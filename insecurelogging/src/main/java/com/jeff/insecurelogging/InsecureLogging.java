26b8b0e65b11305cb27cc38cff18a267f71231e5
Jefferson Capovilla <jefcap@gmail.com>
2020-05-02 20:18:13 -0300

    Insecure Logging
    Objective: Find out what is being logged where/how and the vulnerable code.
    Hint: Insecure logging occurs when developers intentionally or
          unintentionally log sensitive information such as credentials,
          session IDs, financial details etc.

--- insecurelogging/src/main/java/com/jeff/insecurelogging/InsecureLogging.java
+++ insecurelogging/src/main/java/com/jeff/insecurelogging/InsecureLogging.java
@@ -3,6 +3,10 @@ package com.jeff.insecurelogging;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InsecureLogging extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class InsecureLogging extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insecure_logging);
+    }
+
+
+    public void checkout(View view) {
+        EditText cctxt = findViewById(R.id.ccText);
+        try {
+            // Assuming we do some HTTP requests credit card validation and processing
+            //Everything seems fine and then we hit some unforeseen error
+            processCC(cctxt.getText().toString());
+        } catch (RuntimeException re) {
+            Log.e("divax-log", "Error while processing transaction with credit card: " + cctxt.getText().toString());
+            Toast.makeText(this, "An error occured. Please try again later", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void processCC(String toString) {
+        // Do some important processing and throw if there is any error
+        RuntimeException e = new RuntimeException();
+        throw e;
     }
 }
