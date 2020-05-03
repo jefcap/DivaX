540dae2f389fb53daebbe891587c9e325db971b8
Jefferson Capovilla <jefcap@gmail.com>
2020-05-03 13:18:19 -0300

    Hardcoding Issues - Part 1
    Objective: Find out what is hardcoded and where.
    Hint: Developers sometimes will hardcode sensitive information for ease.

--- hardcodingissue1/src/main/java/com/jeff/hardcodingissue1/hardcodingIssue1.java
+++ hardcodingissue1/src/main/java/com/jeff/hardcodingissue1/hardcodingIssue1.java
@@ -3,6 +3,9 @@ package com.jeff.hardcodingissue1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class hardcodingIssue1 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class hardcodingIssue1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hardcoding_issue1);
+    }
+
+    public void access(View view) {
+        EditText hckey = findViewById(R.id.hcKey);
+        String verySecretKey = "vendorsecretkey";
+
+        if (hckey.getText().toString().equals(verySecretKey)) {
+            Toast.makeText(this, "Access granted! See you on the other side :)", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Access denied! See you in hell :D", Toast.LENGTH_SHORT).show();
+        }
     }
 }
