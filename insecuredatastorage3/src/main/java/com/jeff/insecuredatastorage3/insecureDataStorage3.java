e64b73b9a252b2b3978279289cef86d90ea20260
Jefferson Capovilla <jefcap@gmail.com>
2020-05-03 17:14:45 -0300

    Insecure Data Storage - Part 3
    Used createTempFile for data storage
    Objective: Find out where/how the credentials are being stored and the vulnerable code.
    Hint: Insecure data storage is the result of storing confidential information insecurely
          on the system i.e. poor encryption, plain text, access control issues etc.

--- insecuredatastorage3/src/main/java/com/jeff/insecuredatastorage3/insecureDataStorage3.java
+++ insecuredatastorage3/src/main/java/com/jeff/insecuredatastorage3/insecureDataStorage3.java
@@ -3,6 +3,14 @@ package com.jeff.insecuredatastorage3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class insecureDataStorage3 extends AppCompatActivity {
 
@@ -10,5 +18,26 @@ public class insecureDataStorage3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insecure_data_storage3);
+    }
+
+    public void saveCredentials(View view) {
+        EditText usr = findViewById(R.id.ids3Usr);
+        EditText pwd = findViewById(R.id.ids3Pwd);
+
+        File ddir = new File(getApplicationInfo().dataDir);
+
+        try {
+            File uinfo = File.createTempFile("uinfo", "tmp", ddir);
+            uinfo.setReadable(true);
+            uinfo.setWritable(true);
+            FileWriter fw = new FileWriter(uinfo);
+            fw.write(usr.getText().toString() + ":" + pwd.getText().toString() + "\n");
+            fw.close();
+            Toast.makeText(this, "3rd party credentials saved successfully!", Toast.LENGTH_SHORT).show();
+            // Now you can read the temporary file where ever the credentials are required.
+        } catch (IOException e) {
+            Toast.makeText(this, "File error occurred", Toast.LENGTH_SHORT).show();
+            Log.d("DivaX", "File error: " + e.getMessage());
+        }
     }
 }
