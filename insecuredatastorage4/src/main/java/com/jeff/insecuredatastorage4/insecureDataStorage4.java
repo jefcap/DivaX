7181a0e3ae204306486e941969dbd3cfa5e7454a
Jefferson Capovilla <jefcap@gmail.com>
2020-05-03 17:46:52 -0300

    Insecure Data Storage - Part 4
    Used getExternalStorageDirectory for data storage.
    Method is deprecated in Android Q: https://developer.android.com/reference/android/os/Environment#getExternalStorageDirectory()
    Objective: Find out where/how the credentials are being stored and the vulnerable code.
    Hint: Insecure data storage is the result of storing confidential information insecurely
          on the system i.e. poor encryption, plain text, access control issues etc.

--- insecuredatastorage4/src/main/java/com/jeff/insecuredatastorage4/insecureDataStorage4.java
+++ insecuredatastorage4/src/main/java/com/jeff/insecuredatastorage4/insecureDataStorage4.java
@@ -3,6 +3,15 @@ package com.jeff.insecuredatastorage4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class insecureDataStorage4 extends AppCompatActivity {
 
@@ -10,5 +19,26 @@ public class insecureDataStorage4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insecure_data_storage4);
+    }
+
+    public void saveCredentials(View view) {
+        EditText usr = findViewById(R.id.ids4Usr);
+        EditText pwd = findViewById(R.id.ids4Pwd);
+
+        File sdir = Environment.getExternalStorageDirectory();  //Doesn't work in Android Q (https://developer.android.com/reference/android/os/Environment#getExternalStorageDirectory())
+        Log.d("DivaX", "File path: " + sdir.getAbsolutePath());
+
+        try {
+            File uinfo = new File(sdir.getAbsolutePath() + "/.uinfo.txt");
+            uinfo.setReadable(true);
+            uinfo.setWritable(true);
+            FileWriter fw = new FileWriter(uinfo);
+            fw.write(usr.getText().toString() + ":" + pwd.getText().toString() + "\n");
+            fw.close();
+            Toast.makeText(this, "3rd party credentials saved successfully!", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "File error occurred. Example not working in Android Q", Toast.LENGTH_SHORT).show();
+            Log.d("DivaX", "File error: " + e.getMessage());
+        }
     }
 }
