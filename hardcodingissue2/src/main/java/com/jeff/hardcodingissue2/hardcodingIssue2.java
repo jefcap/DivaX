f01f3534793bdecfcf58bf190e5d3292fb7bf8c7
Jefferson Capovilla <jefcap@gmail.com>
2020-05-03 13:33:26 -0300

    Hardcoding Issues - Part 2
    Created stub for JNI example using hardcoded passkey.
    Objective: Find out what is hardcoded and where.
    Hint: Developers sometimes will hardcode sensitive information for ease.

--- hardcodingissue2/src/main/java/com/jeff/hardcodingissue2/hardcodingIssue2.java
+++ hardcodingissue2/src/main/java/com/jeff/hardcodingissue2/hardcodingIssue2.java
@@ -3,6 +3,8 @@ package com.jeff.hardcodingissue2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class hardcodingIssue2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class hardcodingIssue2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hardcoding_issue2);
+    }
+
+    public void access(View view) {
+        Toast.makeText(this, "Method not ready yet. It'll use JNI call", Toast.LENGTH_SHORT).show();
     }
 }
